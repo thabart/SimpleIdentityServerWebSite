765444abf040548d82687290a3979192c56a5162
Thierry Habart <habarthierry@hotmail.fr>
2016-03-24 16:03:13 +0100

    Change WebPack

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,44 @@
+const metadata = {
+    title: 'Simple Identity Server',
+    baseUrl: '/',
+    host: 'localhost',
+    port: 3000,
+    ENV: 'development'
+};
+
+var path = require('path'),
+    srcPath = path.join(__dirname, 'src'),
+    webpack = require('webpack'),
+    HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
-  entry: './src/main.ts',
+  metadata: metadata,
+  entry: {
+      'vendors': path.join(srcPath, 'vendors.ts'),
+      'main': path.join(srcPath, 'main.ts')
+  },
   output: {
       path: 'builds',
-      filename: 'bundle.js'
+      filename: '[name].js',
+      sourceMapFileName: '[name].js.map',
+      chunkFileName: '[id].chunk.js'
   },
   module: {
       loaders: [
           {
               test: /\.ts$/,
-              loader: 'ts'
-          },
-          {
-              test: /\.html$/,
-              loader: 'raw'
+              loader: 'awesome-typescript'
           }
       ]
+  },
+  plugins: [
+      new HtmlWebpackPlugin({
+          template: 'src/index.html',
+          chunksSortMode: 'none'
+      })
+  ],
+  devServer: {
+      port: metadata.port,
+      host: metadata.host
   }
 };
