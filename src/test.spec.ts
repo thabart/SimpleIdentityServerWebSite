8cc8ffb27e995a8e5625d0eaedf1d5ae29c81aad
Thierry Habart <habarthierry@hotmail.fr>
2016-03-29 13:33:44 +0200

    Ticket 3 : Add fake UT

---
+++ src/test.spec.ts
@@ -0,0 +1,15 @@
+/// <reference path="../node_modules/angular2/typings/browser.d.ts" />
+
+import {
+    it,
+    inject,
+    injectAsync,
+    beforeEachProviders,
+    TestComponentBuilder
+} from 'angular2/testing';
+
+describe('App', () => {
+    it('should be ok', function() {
+        expect('true').toEqual('true');
+    });     
+});
